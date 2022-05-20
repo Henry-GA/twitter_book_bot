eb5e4d53c6a78adb48624919b20306d44d241938
Henry Gonzalez <hgonzalez96@outlook.com>
2022-05-19 20:54:24 -0400

    creating data.py file which is going to get the data to tweet

---
+++ data.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+from random import randint
+
+offset = randint(0, 5538)
+html_doc = requests.get(f"http://biblioteca.agn.gob.do/cgi-bin/koha/opac-search.pl?idx=su&q=historia%20dominicana&offset={offset}&sort_by=relevance_dsc")
+soup = BeautifulSoup(html_doc.text,'html.parser')
+
+book_data = soup.find("a", "title").text.split(" /")
+tweet = f"Titulo: {book_data[0]} \n Autor: {book_data[1]}"
+print(tweet)
