aeb858dc43adf09ac41690556bfbd8ea7732ffc3
Henry Gonzalez <hgonzalez96@outlook.com>
2022-05-21 16:25:08 -0400

    return the tweet data from data.py to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tweepy
 import os
+import data
 
 consumer_key = os.getenv("CONSUMER_KEY_TWITTER")
 consumer_secret = os.getenv("CONSUMER_SECRET_TWITTER")
@@ -9,8 +10,10 @@ acces_token_secret = os.getenv("ACCESS_TOKEN_SECRET_TWITTER")
 client = tweepy.Client(consumer_key=consumer_key, consumer_secret=consumer_secret,
                        access_token=access_token, access_token_secret=acces_token_secret)
 
+tweet = data.tweet()
 response = client.create_tweet(
-    text='testing tweepy'
+    text=tweet
 )
 
 print(f"https://twitter.com/user/status/{response.data['id']}")
+
